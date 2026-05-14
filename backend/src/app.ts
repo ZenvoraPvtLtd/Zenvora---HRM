c72021e9785342d5f0c519fd7e712178e6e8402b
Divya yadav <divyayadav141203@gmail.com>
2026-05-14 17:23:05 +0530

    fix the config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,11 +5,8 @@ import path from "path";
 
 import authRoutes from "./routes/auth.routes";
 import oauthRoutes from "./routes/oauth.routes";
-<<<<<<< HEAD
 import jobRoutes from "./routes/job.routes";
-=======
 import candidateRoutes from "./routes/candidate.routes";
->>>>>>> 53a0b0fb5dae36a2d28e8bcbf4f552b16a1db318
 
 const app = express();
 
@@ -21,19 +18,13 @@ app.use(cors({
 app.use(express.json());
 
 app.use(cookieParser());
-<<<<<<< HEAD
 app.use("/api/oauth", oauthRoutes);
-=======
->>>>>>> 53a0b0fb5dae36a2d28e8bcbf4f552b16a1db318
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use("/api/auth", oauthRoutes);
 app.use("/api/auth", authRoutes);
-<<<<<<< HEAD
 app.use("/api/jobs", jobRoutes);
-=======
 app.use("/api/candidate", candidateRoutes);
->>>>>>> 53a0b0fb5dae36a2d28e8bcbf4f552b16a1db318
 
 export default app;
